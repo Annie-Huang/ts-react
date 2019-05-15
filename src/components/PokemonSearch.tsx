8e6ce4e56887b27ed870925c301b1c82385476ed
Annie Huang <annie.hn@gmail.com>
2019-05-15 22:12:11 +1000

    (still not working) create initial search state

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 import User from '../interfaces/User.interface';
 
-class PokemonSearch extends Component<User> {
+interface SearchState {
+    name: string,
+    numberOfAbilites: number,
+    baseExperience: number,
+    imageUrl: string
+}
+
+class PokemonSearch extends Component<User, SearchState> {
     pokemonRef: React.RefObject<HTMLInputElement>;
     constructor(props: User) {
         super(props);
+        this.state = {
+            name: '',
+            numberOfAbilites: 0,
+            baseExperience: 0,
+            imageUrl: ''
+        };
         this.pokemonRef = React.createRef();
     }
 
