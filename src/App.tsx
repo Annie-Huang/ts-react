d271e43f22693fb8506392d5c7a8cbc399e3115b
Annie Huang <annie.hn@gmail.com>
2019-05-15 21:51:54 +1000

    Abtract the property into a interface and one numberOfPokemons optional. Test and it works

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <PokemonSearch name="John Doe" numberOfPokemons={5} />
+      <PokemonSearch name="John Doe" />
     </div>
   );
 }
--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import User from '../interfaces/User.interface';
 
-class PokemonSearch extends Component<{name: string, numberOfPokemons: number}> {
+class PokemonSearch extends Component<User> {
     render() {
         const {name, numberOfPokemons} = this.props;
         return (
